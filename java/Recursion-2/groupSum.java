db3081f8934c6db8e6d215842daaed322b4f5d64
Kyle-Derbabian <77030736+Kyle-Derbabian@users.noreply.github.com>
2021-05-18 21:05:28 -0500

    Create groupSum.java

---
+++ java/Recursion-2/groupSum.java
@@ -0,0 +1,3 @@
+public boolean groupSum(int start, int[] nums, int target) {
+  return start >= nums.length ? target == 0 : groupSum(start + 1, nums, target - nums[start]) || groupSum(start + 1, nums, target);
+}
