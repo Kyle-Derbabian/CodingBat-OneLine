16665a982e30a65924edb291263aca0b8475db08
Kyle-Derbabian <77030736+Kyle-Derbabian@users.noreply.github.com>
2021-05-30 02:31:24 -0500

    Update twoTwo.java

--- java/Array-2/twoTwo.java
+++ java/Array-2/twoTwo.java
@@ -1,17 +1,3 @@
-// Doens't work yet
-
 public boolean twoTwo(int[] nums) {
-  String arrString = Arrays.toString(nums);
-  arrString = arrString.substring(1, arrString.length() - 1);
-  arrString = arrString.replaceAll(",", "");
-
-
-  String line = arrString;
-  String pattern = ".";
-
-  java.util.regex.Pattern r1 = java.util.regex.Pattern.compile(pattern + " 2 " + pattern);
-  java.util.regex.Matcher m1 = r1.matcher(line.substring(1, line.length() - 1));
-
-  return !m1.find();
-  
+  return nums.length == 2 ? !(nums[0] == 2 ^ nums[1] == 2) : nums.length < 2 ? !(Arrays.binarySearch(nums, 2) > -1) : nums[0] == 2 && nums[1] != 2 ? false : twoTwo(Arrays.copyOfRange(nums, nums[0] == 2 && nums[2] != 2 ? 2 : 1, nums.length));
 }
