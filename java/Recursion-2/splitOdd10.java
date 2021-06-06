7269ba12885f18637df8c41f31b7852b2db15fdc
Kyle-Derbabian <77030736+Kyle-Derbabian@users.noreply.github.com>
2021-06-06 00:18:37 -0500

    Update splitOdd10.java

--- java/Recursion-2/splitOdd10.java
+++ java/Recursion-2/splitOdd10.java
@@ -1,7 +1,3 @@
 public boolean splitOdd10(int[] nums) {
-  return helperMethod(0, nums, 0, 0);
-}
-
-public boolean helperMethod(int start, int[] nums, int sum1, int sum2) {
-  return start >= nums.length ? sum1 % 10 == 0 && sum2 % 2 == 1 || sum1 % 2 == 1 && sum2 % 10 == 0 : helperMethod(start + 1, nums, sum1 + nums[start], sum2) || helperMethod(start + 1, nums, sum1, sum2 + nums[start]);
+  return Arrays.stream(nums).filter(i -> i % 2 == 1 && i % 5 == 0).toArray().length % 2 != Arrays.stream(nums).filter(i -> i % 2 == 1 && i % 5 != 0).toArray().length % 2;
 }
